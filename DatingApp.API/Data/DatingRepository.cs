2c73c4e5563e001a79e0f4c18d14b617abe3b6dd
Misa Yednap <misa.yednap@gmail.com>
2020-05-27 18:44:35 -0400

    End of Section 8

---
+++ DatingApp.API/Data/DatingRepository.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DatingRepository : IDatingRepository
+    {
+        private readonly DataContext _context;
+        public DatingRepository(DataContext context)
+        {
+            this._context = context;
+
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            //Include makes sure that the photos are returned
+            //Photos will not be incuded by default becuase it is considered "Navigation" property
+            var user = await  _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
+
+            return users;
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            // if the number of saves > 0 then returns true, if nothing is saved, then returns false
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
