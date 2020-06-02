892385a4c8bdb7372ffe252ae236d5a9770b7968
Misa Yednap <misa.yednap@gmail.com>
2020-06-02 12:53:46 -0400

    End of Section 9

--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -9,7 +9,6 @@ namespace DatingApp.API.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
